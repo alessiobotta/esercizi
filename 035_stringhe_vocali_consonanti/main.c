6f995f9a0a1afb84bf72831ae5ee6b92be4c5b6b
Alessio Botta <a.botta@unina.it>
2025-11-18 22:30:18 +0100

    prova da vscodium

--- 035_stringhe_vocali_consonanti/main.c
+++ 035_stringhe_vocali_consonanti/main.c
@@ -12,10 +12,10 @@ int main() {
     printf("inserisci una stringa: \n");
     scanf("%s", s);
     
-    while(s[i]!= '\0'){
+    while(s[i] != '\0'){
         
-        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' \
-                || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U' ){
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' \
+                || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' ){
         
             vocali[j] = s[i];
             j++;
